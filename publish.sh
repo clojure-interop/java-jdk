fcbbdeffe9bdbf4e506815c5136a427e04449485
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-07-17 19:18:19 +0200

    1.0.0

---
+++ publish.sh
@@ -0,0 +1,150 @@
+cd java.util.spi
+lein deploy clojars
+cd ..
+cd java.util.prefs
+lein deploy clojars
+cd ..
+cd java.util.regex
+lein deploy clojars
+cd ..
+cd java.util.zip
+lein deploy clojars
+cd ..
+cd java.util.logging
+lein deploy clojars
+cd ..
+cd java.util.function
+lein deploy clojars
+cd ..
+cd java.util.jar
+lein deploy clojars
+cd ..
+cd java.util.concurrent
+lein deploy clojars
+cd ..
+cd java.util.stream
+lein deploy clojars
+cd ..
+cd java.lang.reflect
+lein deploy clojars
+cd ..
+cd java.lang.annotation
+lein deploy clojars
+cd ..
+cd java.lang.management
+lein deploy clojars
+cd ..
+cd java.lang.invoke
+lein deploy clojars
+cd ..
+cd java.lang.instrument
+lein deploy clojars
+cd ..
+cd java.lang.ref
+lein deploy clojars
+cd ..
+cd java.net
+lein deploy clojars
+cd ..
+cd java.rmi
+lein deploy clojars
+cd ..
+cd java.time
+lein deploy clojars
+cd ..
+cd java.awt
+lein deploy clojars
+cd ..
+cd java.sql
+lein deploy clojars
+cd ..
+cd java.io
+lein deploy clojars
+cd ..
+cd java.security
+lein deploy clojars
+cd ..
+cd java.beans
+lein deploy clojars
+cd ..
+cd java.lang
+lein deploy clojars
+cd ..
+cd java.applet
+lein deploy clojars
+cd ..
+cd java.math
+lein deploy clojars
+cd ..
+cd java.util
+lein deploy clojars
+cd ..
+cd java.text
+lein deploy clojars
+cd ..
+cd java.nio
+lein deploy clojars
+cd ..
+cd javax.swing
+lein deploy clojars
+cd ..
+cd javax.naming
+lein deploy clojars
+cd ..
+cd javax.annotation
+lein deploy clojars
+cd ..
+cd javax.imageio
+lein deploy clojars
+cd ..
+cd javax.management
+lein deploy clojars
+cd ..
+cd javax.sound
+lein deploy clojars
+cd ..
+cd javax.security
+lein deploy clojars
+cd ..
+cd javax.activation
+lein deploy clojars
+cd ..
+cd javax.jws
+lein deploy clojars
+cd ..
+cd javax.net
+lein deploy clojars
+cd ..
+cd javax.sql
+lein deploy clojars
+cd ..
+cd javax.accessibility
+lein deploy clojars
+cd ..
+cd javax.transaction
+lein deploy clojars
+cd ..
+cd javax.tools
+lein deploy clojars
+cd ..
+cd javax.script
+lein deploy clojars
+cd ..
+cd javax.rmi
+lein deploy clojars
+cd ..
+cd javax.xml
+lein deploy clojars
+cd ..
+cd javax.activity
+lein deploy clojars
+cd ..
+cd javax.lang
+lein deploy clojars
+cd ..
+cd javax.crypto
+lein deploy clojars
+cd ..
+cd javax.print
+lein deploy clojars
+cd ..
